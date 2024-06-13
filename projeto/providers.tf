5744a8c7ed57874b0758024ce2289b2030d6f0b3
Alisson Cruz <alissonpdc@gmail.com>
2024-06-13 12:30:24 -0300

    add: providers.tf

---
+++ projeto/providers.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = {
+        managed-by = "terraform"
+        github-repo = "alissonpdc/curso-terraform-basico-ao-avancado"
+        environment = "dev"
+    }
+  }
+}
--- projeto/versions.tf
+++ projeto/versions.tf
@@ -1,4 +1,9 @@
-# terraform deve estar na versão 1.8.x
 terraform {
   required_version = "~>1.8.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.53.0"
+    }
+  }
 }
