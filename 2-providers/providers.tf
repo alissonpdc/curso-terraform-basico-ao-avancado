8289262c9639cc081e7e7f856c5426591161745c
Alisson Cruz <alissonpdc@gmail.com>
2024-06-13 12:32:42 -0300

    chore: add default_tags & argument reference

--- 2-providers/providers.tf
+++ 2-providers/providers.tf
@@ -1,11 +1,19 @@
 # autenticação do provider "aws"
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs#authentication-and-configuration
 
+# provider: argument reference
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs#argument-reference
+
 # provider padrão
 provider "aws" {
   region = "us-east-1"
   # access_key = "my-access-key"
   # secret_key = "my-secret-key"
+  # default_tags {
+  #   tags = {
+  #     environment = "dev"
+  #   }
+  # }
 }
 
 # provider com alias "west"
